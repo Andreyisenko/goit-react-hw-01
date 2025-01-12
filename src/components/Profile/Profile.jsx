4918f5a28c1a432e20fc5d0253de6d695cfd9b22
Andrey Isenko <aisenko87@gmail.com>
2025-01-12 20:57:25 +0200

    add task-1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+import css from './Profile.module.css'
+const Profile = ({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={css.wraperAvatar}>
+      <div className={css.wraperAvatarNext}>
+        <img className={css.imag} src={image} alt={name} />
+        <p className={css.nameProfile}>{name}</p>
+        <p className={css.parProfile}>{tag}</p>
+        <p className={css.parProfile}> {location}</p>
+      </div>
+
+      <ul className={css.listProfile}>
+        <li className={css.listItemProfile}>
+          <span>Followers</span> 
+
+          <span className={css.listItemSpnProfile}>{followers}</span>
+        </li>
+        <li className={css.listItemProfile}>
+          <span>Views</span>
+
+          <span className={css.listItemSpnProfile}>{views}</span>
+        </li >
+        <li className={css.listItemProfile}>
+          <span>Likes</span>    
+
+          <span className={css.listItemSpnProfile}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Profile;
